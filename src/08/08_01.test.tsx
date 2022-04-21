c143fa7ec347bb618b97df9bcdbd1300c550bc3d
DmitriyKaplan28 <dsk007@mail.ru>
2022-04-21 19:08:53 +0200

    08

--- src/08/08_01.test.tsx
+++ src/08/08_01.test.tsx
@@ -1,13 +1,30 @@
-import {users, usersObj} from "./08_01";
 
-beforeEach( () = > {
+export type UserType = {
+    [key:string] : {id: number, name: string}
+}
 
-})
+let users:UserType
+
+beforeEach (() => {users = {
+        '101': { id:101, name:'a',},
+        '1212': { id:1212, name:'b',},
+        '3232312': { id:3232312, name:'c',},
+        '1': { id:1, name:'d',},
+    }
+}
+)
 
 
 test(" should select corresponding user from obj", () => {
-    expect(usersObj[0]).toBe('a');
-    expect(usersObj[0]).toBe('b');
-    expect(usersObj[0]).toBe('c');
-    expect(usersObj[0]).toBe('d');
+   users['1'].name = 'qwe'
+
+    expect(users['1'].name).toBe('qwe');
+
+})
+
+test(" should delete corresponding user from obj", () => {
+  delete users['1']
+
+    expect(users['1']).toBeUndefined()
+
 })
--- src/08/08_01.tsx
+++ src/08/08_01.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 export const usersArray = ['a', 'b', 'c', 'd']
 
-type UserType = {
-[key:string] : {id: number, name: string}
-}
+
 
 export const usersObj = {
     '0': 'a',
@@ -12,9 +10,9 @@ export const usersObj = {
     '2': 'c',
     '3': 'd'
 }
-export const users:UserType = {
-    '101': { id:101, name:'a',},
-    '1212': { id:1212, name:'b',},
-    '3232312': { id:3232312, name:'c',},
-    '1': { id:1, name:'d',},
-}
+
+/*
+
+let user = {id: 123, name:'f'}
+users[user.id]= user;
+delete users[user.id];*/
