e870aa145a1dcfb8de086cf38e5ce9a0e2c56686
DmitriyKaplan28 <dsk007@mail.ru>
2022-11-11 22:51:28 +0300

    little fix

--- src/08/08_01.tsx
+++ src/08/08_01.tsx
@@ -3,10 +3,9 @@ import React from 'react'
 export const usersArray = ['a', 'b', 'c', 'd']
 
 
-
 export const usersObj = {
     '0': 'a',
-    '1':'b',
+    '1': 'b',
     '2': 'c',
     '3': 'd'
 }
