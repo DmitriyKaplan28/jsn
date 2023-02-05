1e8210532f16d76b6b956a5be8f5a0a7ef5d1187
DmitriyKaplan28 <dsk007@mail.ru>
2023-02-05 22:14:43 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {sentence, splitIntoWords} from "./01-hello-tests/01";
 import {User} from "./06/06-callbacks";
